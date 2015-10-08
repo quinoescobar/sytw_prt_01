911635dd76c96ee007183cc8decf2556987770af
QUINOESCOBAR <quinoescobar@gmail.com>
2015-10-08 17:09:47 +0100

    se modifico el temperatura.js y se  pasaron los test/test.js

--- temperatura.js
+++ temperatura.js
@@ -1,27 +1,31 @@
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 function calcular() {
   var result;
-  var original       = document.getElementById("........");
+  var original = document.getElementById("original");
   var temp = original.value;
-  var regexp = /.............................../;
+  // alert(temp);
+  // Empiece por - +, pude o no puede
+  //Siguiente digito si o si
+  //
+  var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
 
-  var m = temp.match(......);
+  var x = temp.match(regexp);
 
-  if (m) {
-    var num = ....;
-    var type = ....;
+  if (x) {
+    var num = x[1];
+    var type = x[2];
     num = parseFloat(num);
     if (type == 'c' || type == 'C') {
       result = (num * 9/5)+32;
-      result = ..............................
+      result = result.toFixed(1)+" Farenheit";
     }
     else {
       result = (num - 32)*5/9;
-      result = ............................
+      result = result.toFixed(1)+" Celsius";
     }
     converted.innerHTML = result;
   }
   else {
-    converted.innerHTML = "ERROR! Try something like '-4.2C' instead";
+    converted.innerHTML = "¡ERROR! Intente con valores correctos [-,+] [Número] [Medida] e.g: '-4.2C' ";
   }
 }
--- tests/tests.js
+++ tests/tests.js
@@ -9,4 +9,32 @@ suite('temperature', function()
         calcular();
         assert.match(converted.innerHTML, /ERROR/);
     });
+
+    test('34C = 93.2 Farenheit', function()
+    {
+        original.value = "34C";
+        calcular();
+        assert.deepEqual(converted.innerHTML, "93.2 Farenheit");
+    });
+
+    test('34F =  	1.1 Celsius', function()
+    {
+        original.value = "34F";
+        calcular();
+        assert.deepEqual(converted.innerHTML, "1.1 Celsius");
+    });
+
+    test('444c =  831.2 Farenheit', function()
+    {
+        original.value = "444c";
+        calcular();
+        assert.deepEqual(converted.innerHTML, "831.2 Farenheit");
+    });
+
+    test('-43c = -45.4 Farenheit', function()
+    {
+        original.value = "-43c";
+        calcular();
+        assert.deepEqual(converted.innerHTML, "-45.4 Farenheit");
+    });
 });
