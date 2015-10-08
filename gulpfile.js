e54c85d3418fb5f5657c43b2cf9169bbb94bc9ee
QUINOESCOBAR <quinoescobar@gmail.com>
2015-10-08 17:45:25 +0100

    añadido gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp    = require('gulp'),
+    gutil   = require('gulp-util'),
+    uglify  = require('gulp-uglify'),
+    concat  = require('gulp-concat');
+var del     = require('del');
+var minifyHTML = require('gulp-minify-html');
+var minifyCSS  = require('gulp-minify-css');
+
+gulp.task('minify', function () {
+  gulp.src('temperature.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('minified'));
+
+  gulp.src('./index.html')
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('./minified/'))
+
+  gulp.src('./*.css')
+   .pipe(minifyCSS({keepBreaks:true}))
+   .pipe(gulp.dest('./minified/'))
+});
+
+gulp.task('clean', function(cb) {
+  del(['minified/*'], cb);
+});
